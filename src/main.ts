c63a8d75ccd50331cb23b2d3e1a736dbeb06c165
chenming <chenming@uxmc.cn>
2020-12-17 16:25:47 +0800

    更新依赖

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,7 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 
-createApp(App)
-  .use(store)
-  .use(router)
-  .mount('#app');
+createApp(App).use(store).use(router).mount('#app');
 
 console.log(window.__dirname);
 
